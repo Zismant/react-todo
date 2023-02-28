1b5f9282db4e4f877139e9732b533436511c6099
Zismant <100025984+Zismant@users.noreply.github.com>
2023-02-28 20:08:52 +0200

    second start

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
  
-const el = <h1>hello</h1>;
+const el = <h1>hello zalupa777!!!</h1>;
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(el);
