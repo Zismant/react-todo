a5381cf508c6532f35bfd8fa3e531e8e60fe8d64
Zismant <100025984+Zismant@users.noreply.github.com>
2023-03-01 22:24:28 +0200

    add component filter

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,15 +3,30 @@ import "./todo-list-item.css"
 
 const TodoListItem = ({label, important = false}) => {
 
-  const liStyle = {
+  const style = {
     color: important ? 'tomato' : 'black',
+    fontWeight: important ? 'bold' : 'normal',
   }
   return (
-    <span
-      className={'todo-list-item'}
-      style={liStyle}>
-          {label}
+    <span className='todo-list-item'>
+      <span
+        className='todo-list-item-label'
+        style={style}>
+        {label}
+      </span>
+
+      <button type='button'
+              className='btn btn-outline-success btn-sm'>
+        <i className='fa fa-exclamation'/>
+      </button>
+
+      <button type='button'
+              className='btn btn-outline-danger btn-sm'>
+        <i className='fa fa-trash-o'/>
+      </button>
+
     </span>
+
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import TodoList from './components/todo-list/todo-list';
 import AppHeader from './components/app-header/app-header';
 import SearchPanel from "./components/ search-panel/search-panel";
+import ItemStatusFilter from "./components/item-status-filter/item-status-filter";
 
 const App = () => {
 
   const todoData = [
-    { label: 'Drink', important: false, id: 0},
+    { label: 'Drink bear', important: false, id: 0},
     { label: 'Create App', important: false, id: 1},
     { label: 'Drink milk', important: true, id: 2}
   ];
@@ -16,6 +17,7 @@ const App = () => {
     <div>
       <AppHeader />
       <SearchPanel />
+      <ItemStatusFilter />
       <TodoList todos={ todoData }/>
     </div>
   );
